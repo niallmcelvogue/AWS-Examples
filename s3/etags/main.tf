b6613c58c0af00953b860861ff5a1738f49ec931
Niall McElvogue <niallmcelvogue929@hotmail.co.uk>
2024-09-03 14:52:50 +0100

    Provide etag example

---
+++ s3/etags/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "default" {}
+
+resource "aws_s3_object" "object" {
+    bucket = aws_s3_bucket.default.id
+    key    = "myfile.txt"
+    source = "myfile.txt"
+    etag = filemd5("myfile.txt")
+}
